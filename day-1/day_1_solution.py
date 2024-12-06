e3208d14709c56d5b789c36b517adb3356ba8dff
James Ball <jball24-admin@serco.com>
2024-12-06 13:02:06 +0000

    Add day 1 solution

---
+++ day-1/day_1_solution.py
@@ -0,0 +1,26 @@
+from csv import DictReader
+from pandas import (
+    DataFrame,
+    Series,
+)
+from pathlib import Path
+
+import pandas
+
+input_file_path: Path = Path.cwd().joinpath('day-1', 'day_1_input.csv')
+
+unsorted: DataFrame = pandas.read_csv(input_file_path)
+
+x: Series = Series(unsorted['x'])
+y: Series = Series(unsorted['y'])
+
+sorted_x: Series = x.sort_values(ignore_index=True)
+sorted_y: Series = y.sort_values(ignore_index=True)
+
+sorted: DataFrame = pandas.concat([sorted_x, sorted_y], axis=1)
+
+sorted['distance'] = sorted.apply(lambda x: abs(x.loc['x'] - x.loc['y']), axis=1)
+
+total_distance: int = sorted['distance'].sum()
+
+print(f'answer: {total_distance}')
