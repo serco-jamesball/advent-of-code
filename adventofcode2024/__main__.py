2908b9f412e112fbab76452c861e69cc3f6d1147
James Ball <jball24-admin@serco.com>
2024-12-09 15:01:49 +0000

    add day 1 part 2 to main

--- adventofcode2024/__main__.py
+++ adventofcode2024/__main__.py
@@ -1,8 +1,10 @@
 import day1.part1.solution as day1part1
+import day1.part2.solution as day1part2
 
 
 def main() -> None:
     day1part1.main()
+    day1part2.main()
 
 
 if __name__ == "__main__":
